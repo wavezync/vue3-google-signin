494bacf02b49ade11cdb4b0830b9d65b9e1df1a6
Kasun Vithanage <alankasun@gmail.com>
2022-05-16 01:31:23 +0530

    feat(): update typings

--- src/@types/globals.d.ts
+++ src/@types/globals.d.ts
@@ -1,45 +1,52 @@
-interface Window {
-  isLoadingGoogle?: boolean;
-  google?: {
-    accounts: {
-      id: {
-        initialize: (input: IdConfiguration) => void;
-        prompt: (momentListener?: MomenListener) => void;
-        renderButton: (
-          parent: HTMLElement,
-          options: GsiButtonConfiguration,
-          clickHandler?: () => void
-        ) => void;
-        disableAutoSelect: () => void;
-        storeCredential: (
-          credential: { id: string; password: string },
-          callback?: () => void
-        ) => void;
-        cancel: () => void;
-        onGoogleLibraryLoad: () => void;
-        revoke: (accessToken: string, done: () => void) => void;
-      };
-      oauth2: {
-        initTokenClient: (config: TokenClientConfig) => {
-          requestAccessToken: (
-            overridableClientConfig?: OverridableTokenClientConfig
+import {
+  IdConfiguration,
+  MomentListener,
+  GsiButtonConfiguration,
+} from "@/interfaces/accounts";
+
+declare global {
+  interface Window {
+    google?: {
+      accounts: {
+        id: {
+          initialize: (input: IdConfiguration) => void;
+          prompt: (momentListener?: MomentListener) => void;
+          renderButton: (
+            parent: HTMLElement,
+            options: GsiButtonConfiguration,
+            clickHandler?: () => void
           ) => void;
+          disableAutoSelect: () => void;
+          storeCredential: (
+            credential: { id: string; password: string },
+            callback?: () => void
+          ) => void;
+          cancel: () => void;
+          onGoogleLibraryLoad: () => void;
+          revoke: (accessToken: string, done: () => void) => void;
         };
-        initCodeClient: (config: CodeClientConfig) => {
-          requestCode: () => void;
+        oauth2: {
+          initTokenClient: (config: TokenClientConfig) => {
+            requestAccessToken: (
+              overridableClientConfig?: OverridableTokenClientConfig
+            ) => void;
+          };
+          initCodeClient: (config: CodeClientConfig) => {
+            requestCode: () => void;
+          };
+          hasGrantedAnyScope: (
+            tokenRsponse: TokenResponse,
+            firstScope: string,
+            ...restScopes: string[]
+          ) => boolean;
+          hasGrantedAllScopes: (
+            tokenRsponse: TokenResponse,
+            firstScope: string,
+            ...restScopes: string[]
+          ) => boolean;
+          revoke: (accessToken: string, done?: () => void) => void;
         };
-        hasGrantedAnyScope: (
-          tokenRsponse: TokenResponse,
-          firstScope: string,
-          ...restScopes: string[]
-        ) => boolean;
-        hasGrantedAllScopes: (
-          tokenRsponse: TokenResponse,
-          firstScope: string,
-          ...restScopes: string[]
-        ) => boolean;
-        revoke: (accessToken: string, done?: () => void) => void;
       };
     };
-  };
+  }
 }
