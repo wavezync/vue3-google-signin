902c7adbea36db2c4f53b7c41f6b1e399dcd1ed1
Kasun Vithanage <alankasun@gmail.com>
2022-05-16 01:31:42 +0530

    feat(vite): add env vars

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
