f05f3f8859eb9f96448a223367a6802812955111
Kasun Vithanage <alankasun@gmail.com>
2022-05-16 01:33:40 +0530

    chore(): remove unused

--- src/components/GoogleLoginOneTapButton.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import useGsiScript from "../composables/useGsiScript";
-const { loaded } = useGsiScript();
-</script>
-
-<template>
-  <button>Hello btn2 {{ loaded }}</button>
-</template>
--- src/composables/useGoogleLogin.ts
+++
@@ -1,9 +0,0 @@
-import useGsiScript from "./useGsiScript";
-import { watch } from "vue";
-export interface IGoogleLoginConfig {
-  clientId: string;
-}
-
-export default function useGoogleLogin(options: IGoogleLoginConfig) {
-  const { loaded } = useGsiScript();
-}
