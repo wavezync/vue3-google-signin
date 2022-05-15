fa08a7dbd722d391bd419fe53dbfdf3f020b5e36
Kasun Vithanage <alankasun@gmail.com>
2022-05-16 01:30:06 +0530

    feat(plugin): plugin installer

---
+++ src/GoogleOauthPlugin.ts
@@ -0,0 +1,33 @@
+import type { App, Plugin } from "vue";
+import { GoogleClientIdKey } from "@/utils/symbols";
+
+const PLUGIN_NAME = "GoogleOauthPlugin";
+
+export interface GoogleOauthPluginOptions {
+  /**
+   * This field is your application's client ID, which is found and created in the Google Developers Console
+   *
+   * @type {string}
+   * @see https://developers.google.com/identity/gsi/web/guides/get-google-api-clientid
+   * @memberof GoogleOauthPluginOptions
+   */
+  clientId: string;
+}
+
+const plugin: Plugin = {
+  install(app: App, options: GoogleOauthPluginOptions) {
+    // TODO: introduce plugin errors once proposal accepted
+
+    if (!options) {
+      throw new Error(`[${PLUGIN_NAME}]: initialize plugin by passing`);
+    }
+
+    if (!options.clientId) {
+      throw new Error(`[${PLUGIN_NAME}]: clientId is required to initialize`);
+    }
+
+    app.provide(GoogleClientIdKey, options.clientId);
+  },
+};
+
+export default plugin;
