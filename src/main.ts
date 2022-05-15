84c4295c2451cd274804736e0d64cda13fd6120b
Kasun Vithanage <alankasun@gmail.com>
2022-05-16 01:31:54 +0530

    chore: mount plugin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import GoogleOauthPlugin from "./GoogleOauthPlugin";
 
 const app = createApp(App);
+
+app.use(GoogleOauthPlugin, { clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID });
 
 app.mount("#app");
