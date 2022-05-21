ee2d2722d45a82f10c0c3d95077cf0cae97cde35
Kasun Vithanage <alankasun@gmail.com>
2022-05-22 00:33:25 +0530

    chore: update example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import GoogleLoginButton from "./components/GoogleLoginButton.vue";
 import type { CredentialResponse } from "./interfaces/accounts";
+import useCodeClient from "./composables/useCodeClient";
+import { ref } from "vue";
+import useTokenClient from "./composables/useTokenClient";
+
+const scope = ref("");
 
 const onLoginSuccessCallback = (resp: CredentialResponse) => {
   console.log("Login successful", resp);
@@ -9,6 +14,22 @@ const onLoginSuccessCallback = (resp: CredentialResponse) => {
 const onLoginErrorCallback = () => {
   console.log("Login failed");
 };
+
+const { isReady: isCodeClientReady, login: loginCodeClient } = useCodeClient({
+  scope,
+  onSuccess: (resp) => console.log(resp),
+  onError: (resp) => console.error(resp),
+  ux_mode: "popup",
+});
+
+const { isReady: isTokenClientReady, login: loginTokenClient } = useTokenClient(
+  {
+    scope,
+    onSuccess: (resp) => console.log(resp),
+    onError: (resp) => console.error(resp),
+    prompt: "consent",
+  }
+);
 </script>
 
 <template>
@@ -17,4 +38,13 @@ const onLoginErrorCallback = () => {
     @on-login-error="onLoginErrorCallback"
     size="large"
   ></GoogleLoginButton>
+
+  <input placeholder="Scopes" v-model="scope" />
+  <button @click="loginCodeClient" :disabled="!isCodeClientReady">
+    Login Code Client
+  </button>
+
+  <button @click="loginTokenClient" :disabled="!isTokenClientReady">
+    Login Token Client
+  </button>
 </template>
