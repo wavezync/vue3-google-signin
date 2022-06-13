59a8eabd618d3060de7a5cb25a03872e81773918
Kasun Vithanage <alankasun@gmail.com>
2022-06-13 18:44:34 +0530

    chore: update ts config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,45 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import typescript2 from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    typescript2({
+      check: false,
+      include: ["src/components/*.vue"],
+      tsconfigOverride: {
+        compilerOptions: {
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+        exclude: ["vite.config.ts", "main.ts"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: "./src/GoogleOauth2Plugin",
+      formats: ["es", "cjs"],
+      name: "GoogleOauth2Plugin",
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
     },
   },
 });
