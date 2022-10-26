6fd58e394cf4dbe15ff77bce7f63e87f1281ad52
Philippe Da Silva <philippe.dasilva@orizone.io>
2022-10-26 12:02:05 +0200

    chore: function inlining

--- src/utils/oauth2.ts
+++ src/utils/oauth2.ts
@@ -129,9 +129,7 @@ export function decodeJWTToken(jwt: string): JwtToken {
     window
       .atob(base64)
       .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
+      .map((c) => `%${("00" + c.charCodeAt(0).toString(16)).slice(-2)}`)
       .join("")
   );
   const decodedToken = JSON.parse(jsonPayload);
