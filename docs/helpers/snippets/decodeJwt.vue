d8fe2fc3bc04fbffc9cac8277d66ee6227b99305
Philippe Da Silva <philippe.dasilva@orizone.io>
2022-10-26 12:02:26 +0200

    docs: added decodeJWTToken method documentation

---
+++ docs/helpers/snippets/decodeJwt.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { CredentialResponse } from "vue3-google-signin";
+import { GoogleSignIn } from "vue3-google-signin";
+import { decodeJWTToken } from "vue3-google-signin/utils";
+
+const handleSignInSuccess = (response: CredentialResponse) => {
+  const { credential } = response;
+  const decodedCredential = decodeJWTToken(credential);
+  console.log("Acess Token", decodedCredential);
+};
+
+const handleSignInError = () => {
+  console.error("Signin Failed");
+};
+</script>
+
+<template>
+  <GoogleSignInButton
+    @success="handleSignInSuccess"
+    @error="handleSignInError"
+  />
+</template>
